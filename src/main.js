8183591390c3e10fff902ef653922e6e4910547a
Yarik2019 <yroslavterno@gmail.com>
2024-08-06 21:28:13 +0300

    asddsss

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import iziToast from 'izitoast';
 import SimpleLightbox from 'simplelightbox';
 import 'izitoast/dist/css/iziToast.min.css';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-import { getPhotos, perPage } from './js/pixabay-api';
+// import { getPhotos, perPage } from './js/pixabay-api';
 import { imagesMarkup } from './js/render-functions';
 import iconError from './img/error.svg';
 import iconSuccess from './img/success.svg';
