9af8c8fed341b98b614feccdcbc5b2ac8ecfc2ec
Yarik2019 <yroslavterno@gmail.com>
2024-08-06 21:23:58 +0300

    asdd

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,3 +1,25 @@
+// import axios from 'axios';
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+
+// export const perPage = 15;
+
+// export async function getPhotos(q, currentPage) {
+//   const options = {
+//     params: {
+//       key: '45225008-7dd168b8c56fcbfbf82a602af',
+//       q,
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       safesearch: 'true',
+//       page: currentPage,
+//       per_page: perPage,
+//     },
+//   };
+
+//   const { data } = await axios.get('', options);
+
+//   return data;
+// }
 import axios from 'axios';
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 
@@ -6,7 +28,7 @@ export const perPage = 15;
 export async function getPhotos(q, currentPage) {
   const options = {
     params: {
-      key: '45225008-7dd168b8c56fcbfbf82a602af',
+      key: '45170057-222a781b727842b81c5ded16b',
       q,
       image_type: 'photo',
       orientation: 'horizontal',
